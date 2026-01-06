dab3d2d204aaa9db17f4b313e3ec7ac38c7ec536
david-why <david_why@outlook.com>
2026-01-06 17:42:31 +0100

    make app

--- index.ts
+++ index.ts
@@ -3,6 +3,19 @@ import homePage from "./src/index.html";
 
 const app = new Hono().basePath("/api");
 
+app.post("/cards", async (c) => {
+  return c.json({
+    link: `your-app://card/${Bun.randomUUIDv7()}`,
+  });
+});
+
+app.get("/cards/:id", async (c) => {
+  return c.json({
+    email: "",
+    name: "",
+  });
+});
+
 Bun.serve({
   routes: {
     "/*": homePage,
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,62 @@
+import { useState, type FormEvent } from "react";
+
 export function App() {
-    return (
-        <>
-            <p>Hello world :)</p>
-        </>
-    )
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const res = await fetch("/api/cards", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, name }),
+    });
+
+    if (!res.ok) {
+      alert(`Failed to create card: ${await res.text()}`);
+      return;
+    }
+
+    const { link } = await res.json();
+
+    location.href = link;
+  };
+
+  return (
+    <div className="container my-4">
+      <h1 className="mb-2">Create a card</h1>
+      <form onSubmit={onSubmit}>
+        <div className="mb-2">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="name" className="form-label">
+            Full name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          <button className="btn btn-primary">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 }
